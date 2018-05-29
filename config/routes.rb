63b55dc17488641dd1e58ddfee3d16259edbcf3c
Livia <liviaishak1@gmail.com>
2018-05-28 23:59:56 -0700

    api controller added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :registered_applications
   devise_for :users
   root 'welcome#index'
+
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
 end
