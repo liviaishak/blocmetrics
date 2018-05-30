be4508763d8b0767287edc6113bc7c2458c9e8b1
Livia <liviaishak1@gmail.com>
2018-05-30 09:31:19 -0700

    revised API

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  resources :registered_applications
-  devise_for :users
-  root 'welcome#index'
 
   namespace :api, defaults: { format: :json } do
     match '/events', to: 'events#preflight', via: [:options]
     resources :events, only: [:create]
+  end
+
+  resources :registered_applications
+
+  devise_for :users
+
+  root 'welcome#index'
+
 end
